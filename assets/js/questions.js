4bcb80ae4f50d8db4ef54843973cb73fc2171b62
Augustine <smaji08@gmailcom>
2019-12-16 22:12:24 -0500

    a rough model done

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+  {
+    title: "What is the method in JavaScript used to remove the whitespace at the beginning and end of any string?",
+    choices: ["strip()","trim()", "cut()","stripped()"], 
+    answer: "trim()"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+      title: "What is the syntax for creating a function in JavaScript named as callmeFunc?",
+      choices: ["function = callmeFunc()", "function callmeFunc()", "function := callmeFunc()", "function : callmeFunc()"],
+      answer: "function callmeFunc()"
+  }
+];
